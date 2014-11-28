a297b46afea31025f99a0b9e641bebbdd884d7a4
Rich Fellure <rfellure24@gmail.com>
2014-11-28 14:35:22 -0500

    Fixed crash

--- UserDefaults.m
+++ UserDefaults.m
@@ -13,16 +13,17 @@
 ///Sets the default Region.objectId in NSUserDefaults
 +(void)setDefaultRegion:(Region *)theRegion
 {
-    [NSUserDefaults.standardUserDefaults setObject:theRegion.objectId forKey:@"defaultRegionKey"];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:theRegion.objectId forKey:@"defaultRegion"];
     [NSUserDefaults.standardUserDefaults synchronize];
 }
 
 ///Gets the default Region by seeing if there is a default Region, and if there is then doing a query for it based on the stored objectId
 +(void)getDefaultRegionWithBlock:(void (^)(Region *, NSError *))completionHandler
 {
-    if ([NSUserDefaults.standardUserDefaults objectForKey:@"defaultRegionKey"] != nil)
+    if ([NSUserDefaults.standardUserDefaults objectForKey:@"defaultRegion"] != nil)
     {
-        NSString *defaultId = [NSUserDefaults.standardUserDefaults objectForKey:@"defaultRegionkey"];
+        NSString *defaultId = [NSUserDefaults.standardUserDefaults objectForKey:@"defaultRegion"];
         [Region queryForRegionWithObjectId:defaultId completion:^(Region *defaultRegion, NSError *error) {
             completionHandler(defaultRegion, error);
         }];
