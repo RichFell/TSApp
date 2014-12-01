d8825f10a7f5a9c895a5210f045037c1634b15ea
Rich Fellure <rfellure24@gmail.com>
2014-11-30 22:55:53 -0600

    Change of NetworkError to use AlertController
    Refactor so that will meet iOS8 standards and use the UIAlertController versus the alertView

--- TSApp/NetworkErrorAlert.m
+++ TSApp/NetworkErrorAlert.m
@@ -12,9 +12,18 @@
 
 +(void)showNetworkAlertWithError:(NSError *)error withViewController:(UIViewController *)viewController
 {
-    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Sorry there was an error with the Network" message:error.localizedDescription delegate:viewController cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-    [viewController.view addSubview:alertView];
-    [alertView show];
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Sorry there was an error with the network" message:error.localizedDescription preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil];
+    [alertController addAction:okAction];
+    [viewController presentViewController:alertController animated:true completion:nil];
+}
+
++(void)showErrorWithTitle:(NSString *)theTitle withMessage:(NSString *)theMessage forViewController:(UIViewController *)vc
+{
+    UIAlertController *alertController =[UIAlertController alertControllerWithTitle:theTitle message:theMessage preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil];
+    [alertController addAction:okAction];
+    [vc presentViewController:alertController animated:true completion:nil];
 }
 
 @end
