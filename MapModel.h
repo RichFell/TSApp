e25cb08f5d87c74e5cd18cd95a98ffb952460272
Rich Fellure <rfellure24@gmail.com>
2014-12-05 22:58:52 -0600

    Updated Facebook and Parse frameworks

--- MapModel.h
+++ MapModel.h
@@ -13,12 +13,6 @@
 
 @interface MapModel : NSObject
 
-//@property CLPlacemark *searchedPlacemark;
-//@property CLLocationCoordinate2D selectedLocation;
-//@property NSArray *directions;
-//
-//-(CLLocationCoordinate2D)requestSelectedLocation;
-//-(NSArray *)returnDirections;
 +(void)geocodeString:(NSString *)theString withBlock: (void (^)(CLLocationCoordinate2D coordinate, NSError *error))completionHandler;
 +(void)reverseGeoCode: (CLLocationCoordinate2D) location withBlock: (void(^)(GMSReverseGeocodeResponse *response, NSError *error))completionHandler;
 +(void)getDirectionsWithCoordinate: (CLLocationCoordinate2D)startingPosition andEndingPosition: (CLLocationCoordinate2D)endPosition andBlock:(void(^)(NSArray *directionArray, NSError *error))completionHandler;
